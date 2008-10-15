bb035e03d6120660ccf17b41f3cd7e4fe53a3c35
Nick Gerakines <nick@gerakines.net>
2008-10-15 16:55:25 -0700

    Bumping to 0.2. Added patch by Sergei Matusevich for cleaner base32 encoding and decoding.

--- geohash.erl
+++ geohash.erl
@@ -20,30 +20,51 @@
 %% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 %% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 %% OTHER DEALINGS IN THE SOFTWARE.
-
+%% 
+%% Change Log
+%%  * 2008-10-15 ngerakines, v0.2
+%%    - Added edoc friendly documentation.
+%%    - Added a patch submitted by Sergei Matusevich.
+%%    - Misc module organization and layout changes.
+%% 
+%% @author Nick Gerakines <nick@gerakines.net> [http://blog.socklabs.com/]
+%% @copyright 2008 Nick Gerakines
+%% @doc A module that provides basic geohash encoding and decoding.
 -module(geohash).
--compile(export_all).
+
+-export([encode/2, encode/3, decode/1]).
 
 -author("Nick Gerakines <nick@gerakines.net>").
--version("0.1").
+-version("0.2").
+
+-ifdef(TEST).
 
 -include_lib("eunit/include/eunit.hrl").
-
-%% -
-%% Unit tests
 
 geohash_test_() ->
     Tests = [
         {42.6, -5.6, "ezs42"}, {-20, 50, "mh7w"}, {10.1, 57.2, "t3b9m"},
         {49.26, -123.26, "c2b25p"}, {0.005, -179.567, "80021bgm"},
         {-30.55555, 0.2, "k484ht99h2"}, {5.00001, -140.6, "8buh2w4pnt"}],
-    [?_assert(geohash:encode(A, B) == C) || {A, B, C} <- Tests],
-    % [?_assert(geohash:decode(C) == [A, B]) || {A, B, C} <- Tests],
-    ok.
+    [?_assert(encode(A, B) == C) || {A, B, C} <- Tests].
 
-%% -
-%% Base32 encoding
+-endif.
 
+%% @doc Create a hash for a given latitude and longitude.
+encode(Lat, Lon) when is_number(Lat), is_number(Lon) ->
+    Pres = precision(Lat, Lon),
+    encode(Lat, Lon, Pres).
+
+%% @doc Create a hash for a given latitude and longitude with a specific precision.
+encode(Lat, Lon, Pres) when is_number(Lat), is_number(Lon), is_number(Pres) ->
+    encode_major(Pres, {Lat, Lon}, {{90, -90}, {180, -180}}, 1, []).
+
+%% @doc Decode a geohash into a latitude and longitude.
+decode(Hash) when is_list(Hash) ->
+    Set = decode_interval(Hash),
+    [mid(X, Set) || X <- [0, 1]].
+
+%% @private
 base_32() -> [
     {0, $0}, {1, $1}, {2, $2}, {3, $3}, {4, $4}, {5, $5}, {6, $6}, {7, $7},
     {8, $8}, {9, $9}, {10, $b}, {11, $c}, {12, $d}, {13, $e}, {14, $f},
@@ -52,86 +73,82 @@ base_32() -> [
     {29, $x}, {30, $y}, {31, $z}
 ].
 
-encode_base32(X) -> {value, {_, Y}} = lists:keysearch(X, 1, base_32()), Y.
+%% @private
+encode_base32(X) when is_integer(X), X > -1, X < 32 ->
+    {value, {_, Y}} = lists:keysearch(X, 1, base_32()),
+    Y.
 
-decode_base32(X) -> {value, {Y, _}} = lists:keysearch(X, 2, base_32()), Y.
+%% @private
+decode_base32(X) when is_integer(X) ->
+    {value, {Y, _}} = lists:keysearch(X, 2, base_32()),
+    Y.
 
-%% -
-%% encode functionality
-
-encode(Lat, Lon) ->
-    Pres = geohash:precision(Lat, Lon),
-    geohash:encode(Lat, Lon, Pres).
-
-encode(Lat, Lon, Pres) ->
-    geohash:encode_major(Pres, {Lat, Lon}, {{90, -90}, {180, -180}}, 1, []).
-
+%% @private
 encode_major(0, {_Lat, _Lon}, _Set, _Flip, Acc) -> lists:reverse(Acc);
 encode_major(X, {Lat, Lon}, Set, Flip, Acc) ->
-    {Code, NewSet, NewFlip} = geohash:encode_minor(0, {Lat, Lon}, Set, 0, Flip),
+    {Code, NewSet, NewFlip} = encode_minor(0, {Lat, Lon}, Set, 0, Flip),
     encode_major(X - 1, {Lat, Lon}, NewSet, NewFlip, [Code | Acc]).
 
+%% @private
 encode_minor(5, _, Set, Bits, Flip) -> {encode_base32(Bits), Set, Flip};
 encode_minor(X, {Lat, Lon}, Set, Bits, Flip) ->
-    Mid = geohash:mid(Flip, Set),
-    Bit = case geohash:latlon(Flip, {Lat, Lon}) >= Mid of true -> 1; _ -> 0 end,
+    Mid = mid(Flip, Set),
+    Bit = case latlon(Flip, {Lat, Lon}) >= Mid of true -> 1; _ -> 0 end,
     NewBits = (Bits bsl 1) bor Bit,
-    NewSet = geohash:shiftset(Set, Flip, Bit, Mid),
-    encode_minor(X + 1, {Lat, Lon}, NewSet, NewBits, geohash:flip(Flip)).
+    NewSet = shiftset(Set, Flip, Bit, Mid),
+    encode_minor(X + 1, {Lat, Lon}, NewSet, NewBits, flip(Flip)).
 
-%% -
-%% decode functionality
-
-%% [ X / 10000000 || X<-geohash:decode("ezs42")]
-%% [ X / 10000000 || X<-geohash:decode("8buh2w4pnt")]
-decode(Hash) ->
-    Set = decode_interval(Hash),
-    [mid(X, Set) || X <- [0, 1]].
-
+%% @private
 decode_interval(Hash) ->
     M = 1,
     decode_major(Hash, 1, {{90 * M, -90 * M}, {180 * M, -180 * M}}).
 
+%% @private
 decode_major([], _Flip, Set) -> Set;
 decode_major([Char | Chars], Flip, Set) ->
     Bits = decode_base32(Char),
-    {NewSet, NewFlip} = geohash:decode_minor(0, Set, Bits, Flip),
+    {NewSet, NewFlip} = decode_minor(0, Set, Bits, Flip),
     decode_major(Chars, NewFlip, NewSet).
 
+%% @private
 decode_minor(5, Set, _Bits, Flip) -> {Set, Flip};
 decode_minor(X, Set, Bits, Flip) ->
-    Mid = geohash:mid(Flip, Set),
+    Mid = mid(Flip, Set),
     BitPos = (Bits band 16 ) bsr 4,
-    NewSet = geohash:shiftset(Set, Flip, BitPos, Mid),
-    decode_minor(X + 1, NewSet, Bits bsl 1, geohash:flip(Flip)).
+    NewSet = shiftset(Set, Flip, BitPos, Mid),
+    decode_minor(X + 1, NewSet, Bits bsl 1, flip(Flip)).
 
-%% -
-%% private methods
-
+%% @private
 flip(0) -> 1;
 flip(_) -> 0.
 
+%% @private
 shiftset({{A, B}, {_, D}}, 1, BitPos, New) when BitPos == 0 -> {{A, B}, {New, D}};
 shiftset({{A, B}, {C, _}}, 1, 1, New) -> {{A, B}, {C, New}};
 shiftset({{_, B}, {C, D}}, 0, BitPos, New) when BitPos == 0 -> {{New, B}, {C, D}};
 shiftset({{A, _}, {C, D}}, 0, 1, New) -> {{A, New}, {C, D}}.
 
+%% @private
 latlon(0, {X, _}) -> X;
 latlon(1, {_, X}) -> X.
 
+%% @private
 mid(0, {{A, B}, _}) -> (A + B) / 2;
 mid(1, {_, {A, B}}) -> (A + B) / 2.
 
+%% @private
 d2b(X) -> round((X * 3.32192809488736)).
 
+%% @private
 bit_for_number(N) when is_float(N) ->
     [RawChars] = io_lib:fwrite("~f", [N]),
     [_ | [FChars]] = string:tokens(string:strip(RawChars, right, $0), "."),
-    geohash:d2b(length(FChars));
+    d2b(length(FChars));
 bit_for_number(_) -> 0.
 
+%% @private
 precision(Lat, Lon) ->
-    Lab = geohash:bit_for_number(Lat) + 8,
-    Lob = geohash:bit_for_number(Lon) + 9,
+    Lab = bit_for_number(Lat) + 8,
+    Lob = bit_for_number(Lon) + 9,
     Lux = case Lab > Lob of true -> Lab; _ -> Lob end,
     round(Lux / 2.5).
